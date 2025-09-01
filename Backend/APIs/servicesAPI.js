5ff6f9fe35154305280b9d76ea735adef791f7cc
Bello Habeebullah <bellohabeebullah838@gmail.com>
2025-09-01 03:47:28 -0700

    updated sql query to fit railway tables

--- Backend/APIs/servicesAPI.js
+++ Backend/APIs/servicesAPI.js
@@ -1,6 +1,6 @@
 import db from "../Config/db.js";
 const getServices = (req, res)=>{
-   db.query("SELECT * FROM Services", (error, results)=>{
+   db.query("SELECT * FROM services", (error, results)=>{
     if (error) {
       return res.status(500).json({ message: "Internal server error" });
     }
